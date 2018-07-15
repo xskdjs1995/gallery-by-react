838bcf49cbede9ddb4ff3588e17efcfc9e8cd80a
unknown <970654226@qq.com>
2018-07-15 21:46:24 +0800

    add images

--- src/components/Main.js
+++ src/components/Main.js
@@ -10,13 +10,12 @@ let imageDatas = require('../data/imagedata.json');
 /*
 * param： 就是图片的信息对象集合的数组 属性有fileName desc title等
   return： 是url路径 实际路径
-
 */
 imageDatas = (function genImageURL(imageDataArr){
 	for (var i = 0, j = imageDataArr.length; i<j; i++) {
 		var singleImageDate = imageDataArr[i];
 
-		singleImageDate.imageURL= require('../images'+singleImageDate.fileName);
+		singleImageDate.imageURL= require('../images/'+ singleImageDate.fileName);
 		imageDataArr[i] = singleImageDate; // 将原来的对象替换为 添加了imageurl属性的新对象
 	}
 	return imageDataArr;
@@ -26,11 +25,11 @@ imageDatas = (function genImageURL(imageDataArr){
 class AppComponent extends React.Component {
   render() {
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-        <span> 我是谁啊 </span>
-      </div>
+    	<section className="stage">
+    		<section className="img-sec"></section>
+    		<nav className="controller-nav"></nav>
+    	</section>
+      
     );
   }
 }
